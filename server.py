0a169e726e761592d1592487aae561ada7ce72eb
samuele.veneruso <sveneruso@byte-code.com>
2015-04-12 03:59:29 +0200

    Server version 4

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ __author__ = 'Samuele'
 
 from flask import Flask, jsonify
 import mraa
-# import SocketServer
 
 app = Flask(__name__)
 
@@ -23,6 +22,7 @@ def get_fan_status():
 
 @app.route("/fan/<int:new_fan_status>", methods=['POST'])
 def set_fan_status(new_fan_status):
+    global current_fan_status
     if new_fan_status > 0:
         current_fan_status = 1
     else:
@@ -38,6 +38,7 @@ def get_irrigation_status():
 @app.route("/irrigation/<int:new_irrigation_status>", methods=['POST'])
 def set_irrigation_status(new_irrigation_status):
     if new_irrigation_status > 0:
+        global current_irrigation_status
         current_irrigation_status = 1
     else:
         current_irrigation_status = 0
@@ -52,6 +53,7 @@ def get_light_status():
 
 @app.route("/light/<int:new_light_value>", methods=['POST'])
 def set_light_value(new_light_value):
+    global current_light_value
     current_light_value = new_light_value
     return jsonify({'status': current_light_value}), 201
 
@@ -59,38 +61,13 @@ def set_light_value(new_light_value):
 @app.route("/led/on", methods=['GET'])
 def turn_on_led():
     led.write(1)
-    return jsonify({'status', 'LED ON'}), 201
+    return jsonify({'status': 'LED ON'}), 201
 
 
 @app.route("/led/off", methods=['GET'])
 def turn_off_led():
     led.write(0)
-    return jsonify({'status', 'LED OFF'}), 201
-
-# class MyTCPHandler(SocketServer.BaseRequestHandler):
-# """
-#     The RequestHandler class for our server.
-#
-#     It is instantiated once per connection to the server, and must
-#     override the handle() method to implement communication to the
-#     client.
-#     """
-#
-#     def handle(self):
-#         # self.request is the TCP socket connected to the client
-#         self.data = self.request.recv(1024).strip()
-#         print("{} wrote:".format(self.client_address[0]))
-#         print(self.data)
-#         # just send back the same data, but upper-cased
-#         # self.request.sendall(self.data.upper())
-
+    return jsonify({'status': 'LED OFF'}), 201
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-    # HOST, PORT = "192.168.3.7", 5000
-    # Create the server, binding to localhost on port 5000
-    # server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    # server.serve_forever()
