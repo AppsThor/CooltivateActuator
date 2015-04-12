46c9d3e5f70b7e6de7a8dfe92e4c484ca6c169d3
samuele.veneruso <sveneruso@byte-code.com>
2015-04-12 02:45:59 +0200

    Server version 1

---
+++ server.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+__author__ = 'Samuele'
+
+from flask import Flask, jsonify
+import mraa
+
+app = Flask(__name__)
+
+current_fan_status = 0
+current_irrigation_status = 0
+current_light_value = 0
+
+led = mraa.Gpio(4)
+led.dir(mraa.DIR_OUT)
+
+@app.route("/fan", methods=['GET'])
+def get_fan_status():
+    return jsonify({'status': current_fan_status})
+
+@app.route("/fan/<int:new_fan_status>", methods=['POST'])
+def set_fan_status(new_fan_status):
+    if new_fan_status > 0:
+        current_fan_status = 1
+    else:
+        current_fan_status = 0
+    return jsonify({'status': current_fan_status}), 201
+
+@app.route("/irrigation", methods=['GET'])
+def get_irrigation_status():
+    return jsonify({'status': current_irrigation_status})
+
+@app.route("/irrigation/<int:new_irrigation_status>", methods=['POST'])
+def set_irrigation_status(new_irrigation_status):
+    if new_irrigation_status > 0:
+        current_irrigation_status = 1
+    else:
+        current_irrigation_status = 0
+
+    return jsonify({'status': current_irrigation_status}), 201
+
+@app.route("/light", methods=['GET'])
+def get_light_status():
+    return jsonify({'status': current_light_value})
+
+@app.route("/light/<int:new_light_value>", methods=['POST'])
+def set_light_value(new_light_value):
+    current_light_value = new_light_value
+    return jsonify({'status' : current_light_value}), 201
+
+@app.route("/led/on", methods=['GET'])
+def turn_on_led():
+    led.write(1)
+    return jsonify({'status', 'LED ON'}), 201
+
+@app.route("/led/off", methods=['GET'])
+def turn_off_led():
+    led.write(0)
+    return jsonify({'status', 'LED OFF'}), 201
+
+if __name__ == "__main__":
+    app.run()
