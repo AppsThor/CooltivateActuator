c7ef150f32a04c7b1a3bb19ce87ee2f519a38fa7
samuele.veneruso <sveneruso@byte-code.com>
2015-04-12 03:35:08 +0200

    Server version 3

--- server.py
+++ server.py
@@ -59,10 +59,28 @@ def turn_off_led():
     led.write(0)
     return jsonify({'status', 'LED OFF'}), 201
 
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+
 if __name__ == "__main__":
     HOST, PORT = "192.168.3.7", 5000
     # Create the server, binding to localhost on port 5000
-    server = SocketServer.TCPServer((HOST, PORT))
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
