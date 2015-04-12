c4833880ce66d1e5d1491973055959a9a4dae07a
Luciano Quercia <luciano.quercia@gmail.com>
2015-04-12 03:47:59 +0200

    host=0.0.0.0_2

--- server.py
+++ server.py
@@ -67,23 +67,22 @@ def turn_off_led():
     led.write(0)
     return jsonify({'status', 'LED OFF'}), 201
 
-
-class MyTCPHandler(SocketServer.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
-
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        # self.request.sendall(self.data.upper())
+# class MyTCPHandler(SocketServer.BaseRequestHandler):
+# """
+#     The RequestHandler class for our server.
+#
+#     It is instantiated once per connection to the server, and must
+#     override the handle() method to implement communication to the
+#     client.
+#     """
+#
+#     def handle(self):
+#         # self.request is the TCP socket connected to the client
+#         self.data = self.request.recv(1024).strip()
+#         print("{} wrote:".format(self.client_address[0]))
+#         print(self.data)
+#         # just send back the same data, but upper-cased
+#         # self.request.sendall(self.data.upper())
 
 
 if __name__ == "__main__":
